49cbd9801a9c22930efa9bb7df00af4d43b5433a
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2020-08-29 21:47:38 -0500

    just added CL_HPP_TARGET_OPENCL_VERSION to suppress warning msgs

--- clwrap.hpp
+++ clwrap.hpp
@@ -21,6 +21,8 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#define CL_HPP_TARGET_OPENCL_VERSION 120
+#define CL_HPP_MINIMUM_OPENCL_VERSION 120
 #define CL_HPP_ENABLE_PROGRAM_CONSTRUCTION_FROM_ARRAY_COMPATIBILITY
 #include <CL/cl2.hpp>
 
